ff0a93f004928cff6e9c400a9c6929a421bd2779
Remenod <remenod@gmail.com>
2025-11-18 23:17:47 +0200

    remove bootstrap_qemu_serial commented code from `bootstarp/lib`

--- include/arch/x86/paging/bootstrap/lib.h
+++ include/arch/x86/paging/bootstrap/lib.h
@@ -2,15 +2,6 @@
 
 #include <lib/types.h>
 
-// uint8_t bootstrap_inb(uint16_t port);
-// void bootstrap_outb(uint16_t port, uint8_t data);
-// int bootstrap_serial_is_transmit_empty();
-// void bootstrap_serial_write_char(char c);
-// void bootstrap_serial_write_uint8(uint8_t value);
-// void bootstrap_serial_write_uint32(uint32_t value);
-// void bootstrap_serial_write_hex_uint8(unsigned char byte);
-// void bootstrap_serial_write_hex_uint32(uint32_t value);
-
 void bootstrap_set_bitmap8_val(uint8_t bitmap[], size_t index, bool_t val);
 bool_t bootstrap_get_bitmap8_val(uint8_t bitmap[], size_t index);
 void *bootstrap_memcpy(void *dest, const void *src, uint32_t n);
