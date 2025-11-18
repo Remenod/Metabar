2fba7cc3543b38d2ed971b1982338cbcb03bfa55
Remenod <remenod@gmail.com>
2025-11-19 01:41:40 +0200

    remove static from func that should not

--- include/arch/x86/paging/bootstrap/bootstrap_paging.h
+++ include/arch/x86/paging/bootstrap/bootstrap_paging.h
@@ -3,7 +3,7 @@
 #include <lib/types.h>
 #include <paging/page_directory.h>
 
-static void bootstrap_setup_mapping(void);
+void bootstrap_setup_mapping(void);
 void bootstrap_setup_page_directory(void);
 void bootstrap_enable_global_pages(void);
 void bootstrap_enable_paging(void);
